fb2623ebaff3208888756ef952bc09f5241286ed
ziele3920 <209296@student.pwr.edu.pl>
2018-03-26 12:35:11 +0200

    idealnie.pozdrawiam

--- main.m
+++ main.m
@@ -1,13 +1,16 @@
 close all; clear; clc;
 
 rakiFCIRC = ReadRaki('F', 'CIRC');
+dupa = rakiFCIRC(1).image;
+[I, mask] = CutCyc(rakiFCIRC(1).image);
+figure; imshow(uint8(I));
 
 %classesNo = 3;
-%funKmeans = @(block_struct) kMeans(block_struct, classesNo);
+%funKmeans = @(block_struct) kMeans(block_struct.data, classesNo);
 
-%blockWidth = 1024;
-%blockHeight = 20;
+%blockWidth = 20;
+%blockHeight = 1024;
 %I = kMeans(rakiFCIRC(1).image, 2);
 %I2 = blockproc(I, [blockWidth blockHeight], funKmeans);
 %figure;
-%imshow(I2)
+%imagesc(I2)
