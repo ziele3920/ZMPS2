4c51c3167b3780cbaa43b56e3fa1c8cdce12b6bf
ziele3920 <209296@student.pwr.edu.pl>
2018-03-26 12:59:07 +0200

    nie ma nanów.pozdrawiam

---
+++ CutCyc.m
@@ -0,0 +1,41 @@
+function [cutCyc, mask] = CutCyc(image)
+%Algorytm Raka ma byæ funkcja
+%single image feom MIAS database
+k = 2;
+
+Id = double(image(:));
+Ind = kmeans(Id, k);
+Ind = reshape(Ind, [size(image, 1) size(image, 2)]);
+
+%figure; imagesc(Ind);
+
+backround_color = Ind(1,1);
+mask_background = Ind;
+mask_background(mask_background == backround_color) = 0;
+mask_background(mask_background ~= 0) = 1;
+no_background_image = (double(image) .* mask_background);
+
+%figure; imagesc(no_background_image);
+
+k1 = 4;
+Id2 = double(no_background_image(:));
+Ind2 = kmeans(Id2, k1);
+Ind2 = reshape(Ind2, [size(image, 1) size(image, 2)]);
+[maxValue, linearIndexesOfMaxes] = max(no_background_image(:));
+[rowsOfMaxes, colsOfMaxes] = find(no_background_image == maxValue);
+maxValueClassNuber = Ind2(rowsOfMaxes(1), colsOfMaxes(1));
+
+mask_arm = Ind2;
+mask_arm(mask_arm == maxValueClassNuber) = 0;
+mask_arm = mask_arm .* mask_background;
+mask_arm = imfill(mask_arm, 'holes');
+mask_arm(mask_arm ~= 0) = 1;
+mask = mask_arm;
+
+cutCyc = double(image) .* mask_arm;
+%figure; imagesc(Ind2);
+%figure; imagesc(mask_arm);
+%figure; imagesc(cutCyc);
+
+end
+
--- main.m
+++ main.m
@@ -1,16 +1,19 @@
 close all; clear; clc;
 
 rakiFCIRC = ReadRaki('F', 'CIRC');
-dupa = rakiFCIRC(1).image;
-[I, mask] = CutCyc(rakiFCIRC(1).image);
+
+[I, mask] = CutCyc(rakiFCIRC(3).image);
+
+
+classesNo = 4;
+funKmeans = @(block_struct) kMeans(block_struct.data, classesNo);
+
+I = I.^2;
+I = (I - min(I)) ./ ( max(I) - min(I) ) .* 255;
+I(isnan(I)) = 0;
 figure; imshow(uint8(I));
-
-%classesNo = 3;
-%funKmeans = @(block_struct) kMeans(block_struct.data, classesNo);
-
-%blockWidth = 20;
-%blockHeight = 1024;
-%I = kMeans(rakiFCIRC(1).image, 2);
-%I2 = blockproc(I, [blockWidth blockHeight], funKmeans);
-%figure;
-%imagesc(I2)
+blockWidth = 50;
+blockHeight = 1024;
+I2 = blockproc(I, [blockWidth blockHeight], funKmeans);
+figure;
+imagesc(I2)
