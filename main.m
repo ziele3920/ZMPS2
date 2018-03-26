6a23d4ec38c353a6a872fa1e08dd6d05c318440d
ziele3920 <209296@student.pwr.edu.pl>
2018-03-26 13:24:22 +0200

    blur

--- main.m
+++ main.m
@@ -12,6 +12,11 @@ I = I.^2;
 I = (I - min(I)) ./ ( max(I) - min(I) ) .* 255;
 I(isnan(I)) = 0;
 figure; imshow(uint8(I));
+boxKernel = ones(5,5); % Or whatever size window you want.
+I = conv2(I, boxKernel, 'same');
+I = (I - min(I)) ./ ( max(I) - min(I) ) .* 255;
+I(isnan(I)) = 0;
+figure; imshow(uint8(I));
 blockWidth = 50;
 blockHeight = 1024;
 I2 = blockproc(I, [blockWidth blockHeight], funKmeans);
